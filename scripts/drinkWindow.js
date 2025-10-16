81e5334fea964aacb33bbd6da32eb2e8d0b46a0f
KarpX <carpeev.maksimka@yandex.ru>
2025-10-16 18:47:26 +0300

    feat: bs modals, about page

--- scripts/drinkWindow.js
+++ scripts/drinkWindow.js
@@ -1,31 +1,29 @@
-const modal = document.getElementById("drinkWindow");
-const modalTitle = document.getElementById("modalTitle");
+const modal = new bootstrap.Modal(document.getElementById("drinkWindow"));
+const modalTitle = document.getElementById("drinkWindowLabel");
 const modalText = document.getElementById("modalText");
 const modalAlcoholPercent = document.getElementById("alcoholPercentText");
 const modalImg = document.getElementById("modalImg");
-const closeDrinkBtn = document.getElementById("closeModal");
 console.log("loaded");
 document.querySelectorAll(".carousel-item").forEach((card) => {
   card.addEventListener("click", () => {
     console.log("clicked");
-    document.body.style.overflow = "hidden";
     modalTitle.textContent = card.dataset.title;
     modalText.textContent = card.dataset.text;
     modalImg.src = card.dataset.img;
     modalAlcoholPercent.textContent = card.dataset.alcoholPercent;
     console.log(card.dataset.alcoholPercent);
-    modal.showModal();
+    modal.show();
   });
 });
 
-closeDrinkBtn.addEventListener("click", () => {
-  document.body.style.overflow = "";
-  modal.close();
-});
+// closeDrinkBtn.addEventListener("click", () => {
+//   document.body.style.overflow = "";
+//   modal.close();
+// });
 
-modal.addEventListener("click", (e) => {
-  if (e.target === modal) {
-    document.body.style.overflow = "";
-    modal.close();
-  }
-});
+// modal.addEventListener("click", (e) => {
+//   if (e.target === modal) {
+//     document.body.style.overflow = "";
+//     modal.close();
+//   }
+// });
